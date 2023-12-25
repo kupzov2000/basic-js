e48dbbdd49dc27e1085235110a65035c281a030d
Evgeny Kuptsov <kupzov200@mail.ru>
2023-12-26 02:36:24 +0400

    feat: solve the "dream-team" task

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  let membersClear = [];
+  members.forEach((elem) => {
+    if (typeof elem === 'string') {
+      membersClear.push(elem);
+    }
+  })
+
+  let membersClearLetter = [];
+  membersClear.forEach((elem) => membersClearLetter.push(elem.trim()[0].toUpperCase()));
+  membersClearLetter.sort((a, b) => a.localeCompare(b));
+
+  return membersClearLetter.join('');
 }
 
 module.exports = {
